185d2e73581273bccd58ba38a405f1b96f9e72d0
Baro-coder <b.szykula00@gmail.com>
2022-10-22 10:52:34 +0200

    Added: Browser Extension structure

---
+++ browser_extension/js/background.js
@@ -0,0 +1,92 @@
+// FILTERS
+// -------
+// -- HTTPS Sites Filter
+const securePageFilter = { 
+    url: 
+    [
+        { 
+            urlPrefix: "https://"
+        }
+    ]
+}
+
+// -- HTTP Sites Filter
+const insecurePageFilter = { 
+    url: 
+    [
+        { 
+            urlPrefix: "http://"
+        }
+    ]
+}
+
+// LISTENERS
+// ---------
+// -- Page Load Listener - HTTPS - SECURE
+chrome.webNavigation.onCompleted.addListener(() => {
+        handlePageLoad('SECURE')
+    },
+    securePageFilter
+);
+
+// -- Page Load Listener - HTTP - INSECURE
+chrome.webNavigation.onCompleted.addListener(() => {
+        handlePageLoad('INSECURE')
+    },
+    insecurePageFilter
+);
+
+// API
+// ---
+// -- API Endpoint
+const apiUrl = 'http://127.0.0.1:5000/api/url'
+
+// FUNCTIONS
+// ---------
+
+// -- Page Load Handler
+async function handlePageLoad(pageType)
+{
+    const queryOptions = { active: true, lastFocusedWindow: true };
+
+    let tabs = await chrome.tabs.query(queryOptions);
+    if (!tabs.length) return;
+
+    var activeTab = tabs[0];
+    var activeTabUrlFull = activeTab.url; // full url address
+
+    let index = activeTabUrlFull.indexOf('/', 8)
+    var activeTabUrl = activeTabUrlFull.substring(0, index + 1) // only domain address
+
+    var requestBody = JSON.stringify({url : activeTabUrl});
+
+    console.debug('REQUEST:');
+    console.log(requestBody);
+
+    await fetch(apiUrl,
+        {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: requestBody
+        })
+        .then(function(response){ 
+            console.debug(response.status)
+            return response.json(); 
+        })
+        .then(function(data){
+            if (!data.hasOwnProperty("error")) {
+                // RESPONSE STATUS OK
+                console.log(`DOMAIN: ${data.domain}`);
+                console.log(`SCORE:  ${data.phishing_estimate}`);
+            } else {
+                // RESPONSE STATUS ERROR
+                console.log(`URL: ${activeTabUrl}`)
+                console.log(`ERROR: ${data.error}`);
+            }
+        })
+        .catch((reason) => {
+            // NO RESPONSE
+            console.debug(`Error: ${reason}`);
+            console.log(`Error: URL: ${activeTabUrl}`);
+        });
+}
---
+++ browser_extension/js/content.js
@@ -0,0 +1,11 @@
+// -- Error log
+function onError(error) {
+    console.error(`Error: ${error}`)
+}
+
+// LISTENERS
+// ---------
+// -- Runtime Messages Listener
+chrome.runtime.onMessage.addListener((request) => {
+    // TODO: NotImplementedYet
+});
